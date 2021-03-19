c942e6305c2e651a7fe885b2acea40e6ab1ce566
Oliver <oliver_vukovic@yahoo.com>
2021-03-19 22:37:48 +0100

    solution for 6 and 7

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,31 @@
 
 package main;
 
+import student.Student;
+import course.Course;
+import computer.Computer;
+
 public class Main {
 
     public static void main(String[] args) {
        
+       Student ime = new Student();
+       ime.info();
+       
+        
+       ime.setFirstName("Alexa");
+       ime.setSurname("Vukotic");
+       ime.setYear(1980); 
+       
+       
+       
+        
+        
+        
+        
+        
+        
+        
     }
     
 }
--- src/main/java/student/Student.java
+++ src/main/java/student/Student.java
@@ -1,20 +1,30 @@
 package student;
 
+import course.Course;
+import computer.Computer;        
+
+
 public class Student {
     private String firstName;     //ime
-    private String surname;  //prezime
-    private int year;        //god rodjenja
+    private String surname;       //prezime
+    private int year;             //god rodjenja
+    private Course course;
+    private Computer computer;
     
 public Student() {
     this.firstName = "Oliver";
     this.surname = "Vukovic";
     this.year = 1979;
+    this.course = course;
+    this.computer = computer;
 }    
 
-public Student(String firstName, String surname, int year) {
+public Student(String firstName, String surname, int year, Course course, Computer computer) {
     this.firstName = firstName;
     this.surname = surname;
     this.year = year;
+    this.course = course;
+    this.computer = computer;
 }
     
 
@@ -30,7 +40,7 @@ public String getSurname() {
         return this.surname;
 }
 
-public void setSurame(String newSurname) {
+public void setSurname(String newSurname) {
         this.surname = newSurname;
 }        
         
@@ -42,14 +52,35 @@ public void setYear(int newYear) {
         this.year = newYear;    
 }        
 
+public Course getCourse() {
+        return course;
+}
+
+public void setCourse(Course course) {
+        this.course = course;
+}
+
+public Computer getComputer() {
+        return computer;
+}
+
+public void setComputer(Computer computer) {
+        this.computer = computer;
+}
+
 
 public void info() {
     System.out.println("Ime: " + this.getFirstName());
     System.out.println("Prezime: " + this.getSurname());
     System.out.println("Godina rodjenja: " + this.getYear());
-    System.out.println("");
+    System.out.println("Naziv kursa: " + this.getCourse());
+    System.out.println("Broj casova: " + this.getCourse());
+    System.out.println("Procesor: " + this.getComputer() + " GHz");
+    System.out.println("Memorija: " + this.getComputer() + " GB");
+    System.out.println("Hard disk: " + this.getComputer() + " GB");
 }    
-    
+
+   
     
     
     
